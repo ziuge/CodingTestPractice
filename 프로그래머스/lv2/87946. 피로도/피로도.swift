e5429245d62dd0919dd08ebab0c771f6d8f95a69
Jiwon Choi <79574342+ziuge@users.noreply.github.com>
2023-07-14 15:45:45 +0900

    [level 2] Title: 피로도, Time: 0.04 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.swift
+++ 프로그래머스/lv2/87946. 피로도/피로도.swift
@@ -1,49 +1,22 @@
 import Foundation
 
 func solution(_ k:Int, _ dungeons:[[Int]]) -> Int {
-    var cases = permute(Array<Int>(0...dungeons.count-1))
-    var hp = k
-    // var answer = 0
-    var maxDungeons = 0
+    var answer = 0
+    var visited = [Bool](repeating: false, count: dungeons.count)
     
-    cases.forEach { item in
-        hp = k
-        var tempMax = 0
-        for i in 0...item.count - 1 {
-            if dungeons[item[i]][0] <= hp && dungeons[item[i]][1] <= hp { // 최소 필요 피로도, 소모 피로도
-                hp -= dungeons[item[i]][1]
-                tempMax += 1
-            } else {
-                break
-            }
-            maxDungeons = max(tempMax, maxDungeons)
-        }
-    }    
-    
-    return maxDungeons
-}
+    func DFS(_ k: Int, _ count: Int) {
+        answer = max(answer, count)
 
-func permute(_ nums: [Int]) -> [[Int]] {
-    var result: [[Int]] = []
-    var visited = [Bool](repeating: false, count: nums.count)
-
-    func permutation(_ temp: [Int]) {
-        if temp.count == nums.count {
-            result.append(temp)
-        }
-
-        for i in 0...nums.count - 1 {
-            if visited[i] == true {
-                continue
-            } else {
+        for i in 0...dungeons.count - 1 {
+            if visited[i] == false && k >= dungeons[i][0]{
                 visited[i] = true
-                permutation(temp + [nums[i]])
+                DFS(k - dungeons[i][1], count + 1)
                 visited[i] = false
             }
         }
-
     }
-    permutation([])
-
-    return result
+    
+    DFS(k, 0)
+    
+    return answer
 }
