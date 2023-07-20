71356b976f8b395899cf3e03897597c854580434
Jiwon Choi <79574342+ziuge@users.noreply.github.com>
2023-07-20 13:58:46 +0900

    [Bronze III] Title: 배수와 약수, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.swift
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.swift
@@ -1,23 +1,11 @@
 import Foundation
 
-while true {
-    let arr = readLine()!.split(separator: " ").map({ Int($0)! })
-    
-    let n = arr[0]
-    let m = arr[1]
-    
-    if arr[0] == arr[1] {
-        break
+while let nums = readLine()?.split(separator: " ").map({ Int($0)! }), nums != [0, 0] {
+    if nums[1] % nums[0] == 0 {
+        print("factor")
+    } else if nums[0] % nums[1] == 0 {
+        print("multiple")
+    } else {
+        print("neither")
     }
-    
-    var answer = ""
-    
-    answer = n > m ? "multiple" : "factor"
-    
-    if max(n, m) % min(n, m) != 0 {
-        answer = "neither"
-    }
-    
-    print(answer)
-    
 }
