e28505fe465538b98156404382df2a3fcfd92a9d
Jiwon Choi <79574342+ziuge@users.noreply.github.com>
2023-09-08 15:05:38 +0900

    [level 3] Title: 네트워크, Time: 0.11 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.swift
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.swift
@@ -1,40 +1,24 @@
 import Foundation
 
 func solution(_ n:Int, _ computers:[[Int]]) -> Int {
-    
-    var computersDict: [Int: [Int]] = [:]
-    for i in 0..<n {
-        for j in 0..<n {
-            if computers[i][j] == 1 {
-                if computersDict[i] == nil {
-                    computersDict[i] = [j]
-                } else {
-                    computersDict[i]!.append(j)
-                }
-            }
-        }
-    }
-    
+
     var answer = 0
     var visited = Array(repeating: false, count: n)
     
-    func dfs(_ from: Int, _ to: Int) {
+    func dfs(_ to: Int) {
         visited[to] = true
         
         for i in 0..<n {
-            if computersDict[to]!.contains(i) && visited[i] == false {
-                dfs(from, i)
+            if computers[to][i] == 1 && visited[i] == false {
+                dfs(i)
             }
         }
     }
     
     for i in 0..<n {
-        for j in computersDict[i]! {
-            if visited[j] == false {
-                visited[i] = true
-                answer += 1
-                dfs(i, j)
-            }
+        if !visited[i] {
+            answer += 1
+            dfs(i)
         }
     }
     
